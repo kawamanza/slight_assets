86d3d627713acd31bc99059a6072dc09a14daf2f
Marcelo Manzan <manzan@gmail.com>
2011-05-25 09:43:54 -0300

    writing minified asset

--- lib/slight_assets/util.rb
+++ lib/slight_assets/util.rb
@@ -22,6 +22,34 @@ module SlightAssets
     end
     module_function :write_static_gzipped_file
 
+    begin
+      require "yui/compressor"
+      def write_static_minified_asset(file_path)
+        return file_path if file_path.nil? || file_path =~ /\.min\./ || file_path !~ /\.(?:js|css)\z/ || ! File.exists?(file_path)
+        min_path = file_path.gsub(/\.(js|css)\z/, '.min.\1')
+        case extension = $1
+        when "css"
+          compressor = YUI::CssCompressor.new
+        when "js"
+          compressor = YUI::JavaScriptCompressor.new
+        else
+          return file_path
+        end
+        content = File.read(file_path)
+        content = compressor.compress(content)
+        File.open(min_path, "w") { |f| f.write(content) }
+        mt = File.mtime(file_path)
+        File.utime(mt, mt, min_path)
+        min_path
+      end
+    rescue LoadError => e
+      STDERR.puts "WARN: #{e.message}"
+      def write_static_minified_asset(file_path)
+        file_path
+      end
+    end
+    module_function :write_static_minified_asset
+
     def async_write_static_compressed_file(file_path)
       Thread.new do
         file_path = write_static_gzipped_file(file_path)
--- spec/lib/util_spec.rb
+++ spec/lib/util_spec.rb
@@ -3,6 +3,7 @@ require "spec_helper"
 describe "SlightAssets::Util" do
   it "should respond to methods" do
     SlightAssets::Util.should respond_to(:write_static_gzipped_file)
+      SlightAssets::Util.should respond_to(:write_static_minified_asset)
     SlightAssets::Util.should respond_to(:async_write_static_compressed_file)
   end
   context "writing" do
@@ -17,6 +18,15 @@ describe "SlightAssets::Util" do
       output_path.should be == zip_path
       File.should be_exists(zip_path)
       File.delete(zip_path)
+    end
+    it "a minified file" do
+      min_path = @file_path.gsub(/\.(js|css)\z/, '.min.\1')
+      File.delete(min_path) if File.exists?(min_path)
+      File.should_not be_exists(min_path)
+      output_path = SlightAssets::Util.write_static_minified_asset(@file_path)
+      output_path.should be == min_path
+      File.should be_exists(min_path)
+      File.delete(min_path)
     end
   end
 end
