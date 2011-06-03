c0b96eb0bfef8b0316c28e8ec0b8ab82fbd8b334
Marcelo Manzan <manzan@gmail.com>
2011-06-03 18:53:44 -0300

    SlightAssets::Rails: improvements on method 'write_asset_file_contents'

--- lib/slight_assets/rails.rb
+++ lib/slight_assets/rails.rb
@@ -17,9 +17,29 @@ module SlightAssets
       private
 
       def write_asset_file_contents_with_static_compressed_file(joined_asset_path, asset_paths)
-        already_existed = File.exists?(joined_asset_path.gsub(/\.(js|css)\z/, '.min.\1'))
-        r = write_asset_file_contents_without_static_compressed_file(joined_asset_path, asset_paths)
-        SlightAssets::Util.async_write_static_compressed_file(joined_asset_path) unless already_existed
+        minify = false
+        asset_paths = asset_paths.map do |asset_path|
+          p = asset_path.to_s
+          if p =~ /\.min\./
+            asset_path
+          else
+            p = p.gsub(/\.(js|css)(\?\w+)?\z/, '.min.\1\2')
+            if ::File.exists?(asset_file_path(p))
+              p
+            else
+              minify = true
+              asset_path
+            end
+          end
+        end
+        minified_joined_asset_path = joined_asset_path.gsub(/(?:\.min)?\.(js|css)\z/, '.min.\1')
+        already_existed = File.exists?(minified_joined_asset_path)
+        r = write_asset_file_contents_without_static_compressed_file(minify ? joined_asset_path : minified_joined_asset_path, asset_paths)
+        if minify && ! already_existed
+          SlightAssets::Util.async_write_static_compressed_file(joined_asset_path)
+        else
+          SlightAssets::Util.write_static_gzipped_file(minified_joined_asset_path)
+        end
         r
       end
 
@@ -27,11 +47,11 @@ module SlightAssets
         unless source =~ /\A(?:\w+:)?\/\//
           asset_path = path_to_javascript(source).split('?').first
           file_path = asset_file_path(asset_path)
-          if asset_path.end_with?(".js") && asset_path !~ /\.min\./ && ::File.exists?(file_path)
+          if asset_path.end_with?(".js") && asset_path !~ /\.min\./
             compressed_path = asset_path.gsub(/\.js\z/, '.min.js')
             if ::File.exists?(asset_file_path(compressed_path))
               source = compressed_path unless ::File.exists?("#{file_path}.locked")
-            else
+            elsif ::File.exists?(file_path)
               SlightAssets::Util.async_write_static_compressed_file(file_path)
             end
           end
@@ -43,11 +63,11 @@ module SlightAssets
         unless source =~ /\A(?:\w+:)?\/\//
           asset_path = path_to_stylesheet(source).split('?').first
           file_path = asset_file_path(asset_path)
-          if asset_path.end_with?(".css") && asset_path !~ /\.min\./ && ::File.exists?(file_path)
+          if asset_path.end_with?(".css") && asset_path !~ /\.min\./
             compressed_path = asset_path.gsub(/\.css\z/, '.min.css')
             if ::File.exists?(asset_file_path(compressed_path))
               source = compressed_path unless ::File.exists?("#{file_path}.locked")
-            else
+            elsif ::File.exists?(file_path)
               SlightAssets::Util.async_write_static_compressed_file(file_path)
             end
           end
