3ec5e9978d75eaab152816685797d0af5e8ac27d
Marcelo Manzan <manzan@gmail.com>
2011-06-06 10:59:57 -0300

    rake to pre compress CSS assets

--- lib/slight_assets/rake_tasks.rb
+++ lib/slight_assets/rake_tasks.rb
@@ -21,5 +21,23 @@ namespace :asset do
       end
       puts "Total: #{jslist.size} JS files were compressed" if jslist.any?
     end
+
+    desc "Compress all CSS files from your Rails application"
+    task :css => :pre do
+      if SlightAssets::Util.css_compressor.nil?
+        puts "WARN: No CSS compressor was found"
+        exit 1
+      end
+      csslist = FileList[File.join(Rails.root, *%w(public stylesheets ** *.css))].select{|file| ! ( file =~ /\.min\.css$/ || File.exists?(file.gsub(/\.css$/, '.min.css')) ) }
+      csslist.each do |cssfile|
+        puts "Compressing #{cssfile[(Rails.root.to_s.size+1)..-1]}"
+        minfile = SlightAssets::Util.write_static_compressed_file(cssfile).chomp(".gz")
+        csssize = File.size(cssfile)
+        minsize = File.size(minfile)
+        percent = csssize.zero? ? 100 : (100.0 * minsize / csssize).round
+        puts "  -> #{csssize} bytes to #{minsize} bytes (#{percent}% size)"
+      end
+      puts "Total: #{csslist.size} CSS files were compressed" if csslist.any?
+    end
   end
 end
