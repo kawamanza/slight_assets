55d532e32ef53e92cddd25c786dcd11c55c5476d
Marcelo Manzan <manzan@gmail.com>
2011-06-17 12:20:23 -0300

    tiny refactoring on Util

--- lib/slight_assets/util.rb
+++ lib/slight_assets/util.rb
@@ -1,26 +1,19 @@
 require "base64"
+require "zlib"
 
 module SlightAssets
   module Util
-    begin
-      require "zlib"
-      def write_static_gzipped_file(file_path, content = nil)
-        return file_path if file_path.nil? || file_path.end_with?(".gz") || ! File.exists?(file_path)
-        zip_path = "#{file_path}.gz"
-        return zip_path if File.exists?(zip_path)
-        content = File.read(file_path) if content.nil?
-        Zlib::GzipWriter.open(zip_path, Zlib::BEST_COMPRESSION) {|f| f.write(content) }
-        # Set mtime to the latest of the file to allow for
-        # consistent ETag without a shared filesystem.
-        mt = File.mtime(file_path)
-        File.utime(mt, mt, zip_path)
-        zip_path
-      end
-    rescue LoadError
-      STDERR.puts "WARN: Couldn't load Zlib Gem"
-      def write_static_gzipped_file(file_path)
-        file_path
-      end
+    def write_static_gzipped_file(file_path, content = nil)
+      return file_path if file_path.nil? || file_path.end_with?(".gz") || ! File.exists?(file_path)
+      zip_path = "#{file_path}.gz"
+      return zip_path if File.exists?(zip_path)
+      content = File.read(file_path) if content.nil?
+      Zlib::GzipWriter.open(zip_path, Zlib::BEST_COMPRESSION) {|f| f.write(content) }
+      # Set mtime to the latest of the file to allow for
+      # consistent ETag without a shared filesystem.
+      mt = File.mtime(file_path)
+      File.utime(mt, mt, zip_path)
+      zip_path
     end
     module_function :write_static_gzipped_file
 
