ff87919c91001a7188bf6947d1f9277cbfbf0221
Marcelo Manzan <manzan@gmail.com>
2011-06-20 15:09:19 -0300

    improvements on JsReducer

--- lib/slight_assets/js_reducer.rb
+++ lib/slight_assets/js_reducer.rb
@@ -2,28 +2,29 @@ module SlightAssets
   class JsReducer
     BYTE_RANGE = 32..127
     BASE_NUMBER = BYTE_RANGE.count
-    BYTE_COUNT = BASE_NUMBER + 3
     FETCH_RANGE = BASE_NUMBER ** 2
     LOOKUP_CHAR = "`"
+    ESCAPABLE_CHARS = ["\\", "\"", "\r", "\n"].freeze
 
-    def compress(js_content, last_occurrences = true)
-      content = reduce(js_content, last_occurrences)
+    def compress(js_content)
+      content = reduce(js_content)
       return js_content if content == js_content
-      func = 'function(x){var b="",p=0,c,l,L=x.length,m="`";while(p<L)b+=(c=x[p++])!=m?c:(l=x.charCodeAt(p++)-28)>4?b.substr(b.length-x.charCodeAt(p++)*96-x.charCodeAt(p++)+3104-l,l):m;return b}'
+      func = 'function(x){var b="",p=0,c,l,L=x.length,m="`";while(p<L)b+=(c=x[p++])!=m?c:(l=x.charCodeAt(p++)-31)>1?b.substr(b.length-x.charCodeAt(p++)*96-x.charCodeAt(p++)+3104-l,l):m;return b}'
       content = content.gsub(/([\\"])/, '\\\\\\1').gsub(/\r/, "\\r").gsub(/\n/, "\\n")
       "eval((#{func})(\"#{content}\"))"
     end
 
-    def reduce(js_content, last_occurrences = true)
+    def reduce(js_content)
+      max_cropsize = BASE_NUMBER
       content = str_as_utf8(js_content)
       return js_content if content.nil?
       output, index, limit = "", -1, content.size
       while (index+=1) < limit
         i, chars = -1, str_as_utf8("")
-        cropindex, cropsize = nil, 0
-        while (i+=1) < BYTE_COUNT && index + i < limit
-          chars << content[index+i]
-          next if chars.size < 5
+        cropindex, cropsize, s = nil, 0, 0
+        while (i+=1) < max_cropsize && index + i < limit
+          chars << (c = content[index+i])
+          next if s < 5 && (s += (ESCAPABLE_CHARS.include?(c) ? 2 : c.bytesize) ) < 5
           max_start = index - chars.size
           start = [0, max_start - FETCH_RANGE].max
           if (pos = content.index(chars, start)) && pos <= max_start
@@ -34,13 +35,11 @@ module SlightAssets
           output << content[index]
           output << " " if content[index] == LOOKUP_CHAR
         else
-          if last_occurrences
-            max_start = [0, index - cropsize].max
-            chars = str_as_utf8(content[cropindex, cropsize])
-            cropindex = pos while (pos = content.index(chars, cropindex + 1)) && pos <= max_start
-          end
+          max_start = [0, index - cropsize].max
+          chars = str_as_utf8(content[cropindex, cropsize])
+          cropindex = pos while (pos = content.index(chars, cropindex + 1)) && pos <= max_start
           output << LOOKUP_CHAR
-          output << BYTE_RANGE.first - 4 + cropsize
+          output << BYTE_RANGE.last - max_cropsize + cropsize
           offset = (index - cropindex) - cropsize
           output << BYTE_RANGE.first + (offset / BASE_NUMBER)
           output << BYTE_RANGE.first + (offset % BASE_NUMBER)
--- spec/lib/js_reducer_spec.rb
+++ spec/lib/js_reducer_spec.rb
@@ -4,63 +4,23 @@ describe "SlightAssets::JsReducer" do
   it "should reduce JS" do
     from = "function callback1(){var tcallback=function(){return true};return tcallback}"
     from.size.should be == 76
-    to   = "function callback1(){var t`" + [28+8, 32, 32+17-8].pack("ccc") +
-                                             "=`" + [28+8, 32, 32+35-8].pack("ccc") +
-                                                      "(){return true};`" + [28+8, 32, 32+13-8].pack("ccc") +
-                                                                              "`" + [28+8, 32, 32+41-8].pack("ccc") +
+    to   = "function callback1(){var t`" + [31+8, 32, 32+17-8].pack("ccc") +
+                                             "=`" + [31+8, 32, 32+35-8].pack("ccc") +
+                                                      "(){return true};`" + [31+8, 32, 32+13-8].pack("ccc") +
+                                                                              "`" + [31+8, 32, 32+41-8].pack("ccc") +
                                                                                       "}"
     tr = SlightAssets::JsReducer.new.reduce(from)
     tr.should be == to
     tr.size.should be == 60
   end
-  it "should crop 99 characters from '99 chars * n'" do
+  it "should crop 96 characters from '96 chars * n'" do
     str = SlightAssets::JsReducer::BYTE_RANGE.inject(""){ |s, i| i != 96 ? (s << i) : s }
     n = 200
-    from = (str + "jspk") * n
-    from.size.should be == 99 * n
-    to   = (str + "jspk") + ([96, 28+99, 32, 32].pack("cccc") * (n-1))
+    from = (str + "j") * n
+    from.size.should be == 96 * n
+    to   = (str + "j") + ([96, 31+96, 32, 32].pack("cccc") * (n-1))
     tr = SlightAssets::JsReducer.new.reduce(from)
-    tr.size.should be == 99 + 4 * (n - 1)
-    tr.should be == to
-  end
-  it "should crop 99 characters from '100 chars * n'" do
-    str = SlightAssets::JsReducer::BYTE_RANGE.inject(""){ |s, i| i != 96 ? (s << i) : s }
-    n = 200
-    from = (str + "jspk1") * n
-    from.size.should be == 100 * n
-    to   = (str + "jspk1") + ([96, 28+99, 32, 32+1].pack("cccc") * (n+1)) + "1"
-    tr = SlightAssets::JsReducer.new.reduce(from)
-    tr.size.should be == 100 + 4 * (n + 1) + 1
-    tr.should be == to
-  end
-  it "should crop 99 characters from '101 chars * n'" do
-    str = SlightAssets::JsReducer::BYTE_RANGE.inject(""){ |s, i| i != 96 ? (s << i) : s }
-    n = 200
-    from = (str + "jspk13") * n
-    from.size.should be == 101 * n
-    to   = (str + "jspk13") + ([96, 28+99, 32, 32+2].pack("cccc") * (n+3)) + "13"
-    tr = SlightAssets::JsReducer.new.reduce(from)
-    tr.size.should be == 101 + 4 * (n + 3) + 2
-    tr.should be == to
-  end
-  it "should crop 99 characters from '102 chars * n'" do
-    str = SlightAssets::JsReducer::BYTE_RANGE.inject(""){ |s, i| i != 96 ? (s << i) : s }
-    n = 200
-    from = (str + "jspk135") * n
-    from.size.should be == 102 * n
-    to   = (str + "jspk135") + ([96, 28+99, 32, 32+3].pack("cccc") * (n+5)) + "135"
-    tr = SlightAssets::JsReducer.new.reduce(from)
-    tr.size.should be == 102 + 4 * (n + 5) + 3
-    tr.should be == to
-  end
-  it "should crop 99 characters from '103 chars * n'" do
-    str = SlightAssets::JsReducer::BYTE_RANGE.inject(""){ |s, i| i != 96 ? (s << i) : s }
-    n = 200
-    from = (str + "jspk1357") * n
-    from.size.should be == 103 * n
-    to   = (str + "jspk1357") + ([96, 28+99, 32, 32+4].pack("cccc") * (n+7)) + "1357"
-    tr = SlightAssets::JsReducer.new.reduce(from)
-    tr.size.should be == 103 + 4 * (n + 7) + 4
+    tr.size.should be == 96 + 4 * (n - 1)
     tr.should be == to
   end
 end
