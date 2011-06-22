936d5f96b27771c52ebd5025c5b4cf8822bee335
Marcelo Manzan <manzan@gmail.com>
2011-06-22 09:46:10 -0300

    Rails 3 experiment folder

---
+++ experiment/rails3.0.9_root/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rails309Root::Application.config.secret_token = 'e4bf2c9cd3fcbb4d9e31960bae614e8d3d51f20a3b06cdf044696bda36c10192154b4fd49479b362a899af61382207c9de24b55b8482362c14909a5206c352dc'
