8d3bc32560e552f4c6b040558cfb45c769d0e7c3
Marcelo Manzan <manzan@gmail.com>
2011-07-14 10:39:38 -0300

    improvements on version.rb

--- lib/slight_assets/version.rb
+++ lib/slight_assets/version.rb
@@ -1,6 +1,6 @@
 module SlightAssets
   version = nil
   version = $1 if ::File.expand_path('../..', __FILE__) =~ /\/slight_assets-(\d[\w\.]+)/
-  version = ::StepUp::Driver::Git.new.last_version_tag if version.nil? && ::File.exists?(::File.expand_path('../../../.git', __FILE__))
+  version = ::StepUp::Driver::Git.new.last_version_tag rescue "v0.0.0" if version.nil? && ::File.exists?(::File.expand_path('../../../.git', __FILE__))
   VERSION = version.gsub(/^v?([^\+]+)\+?\d*$/, '\1')
 end
